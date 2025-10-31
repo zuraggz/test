63acf42755900f3d677ceb4b1d3442c19452f7c9
Zura Gurabanidze <zura.gurabanidze.1@btu.edu.ge>
2025-10-31 14:30:04 +0400

    Initial commit

---
+++ Banki/Bank.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banki
+{
+    class BankAccount
+    {
+        private static int accN = 1000;
+        public  int accountNumber { get; private set; }
+        public string ownerName { get; set; }
+        public decimal balance { get; private set; } = decimal.Zero;
+
+        public BankAccount(string ownerName, int accountNumber, decimal balance, Bank bank)
+        {
+            this.ownerName = ownerName;
+            this.accountNumber = accN;
+            accN++;
+            this.balance = balance;
+            bank.AddAccount(this);
+        }
+        public BankAccount(string ownerName, Bank bank)
+        {
+            this.ownerName=ownerName;
+            bank.AddAccount(this);
+            accountNumber=accN;
+            accN++;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine("Payment has been done!");
+            }
+
+            else
+            {
+                Console.WriteLine("Enter a valid number!");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && balance >= amount)
+            {
+                balance -= amount;
+                Console.WriteLine("Operation has been done!");
+            }
+            else
+            {
+                
+                Console.WriteLine("Enter the valid ammount");
+            }
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"-------------------------\nOwner name: {ownerName}\nAccount number: {accountNumber}\nBalnace: {balance}");
+
+        }
+
+
+    }
+
+    class Bank
+    {
+        private List<BankAccount> bankAccounts = new List<BankAccount>();
+
+        public void AddAccount(BankAccount account)
+        {
+            bankAccounts.Add(account);
+        }
+
+        private BankAccount FindAccount(int accountNumber)
+        {
+            foreach (BankAccount account in bankAccounts)
+            {
+                if (account.accountNumber == accountNumber)
+                {
+                    return account;
+                }
+
+            }
+
+            return null;
+        }
+
+        public void Transfer(int fromAccount, int toAccount, decimal amount)
+        {
+            BankAccount facc=FindAccount(fromAccount);
+            BankAccount tacc=FindAccount(toAccount);
+
+            facc.Withdraw(amount);
+            tacc.Deposit(amount);
+        }
+
+
+    }
+}
---
+++ Banki/Program.cs
@@ -0,0 +1,12 @@
+﻿using Banki;
+Bank TBC = new Bank();
+BankAccount zuragg = new BankAccount("zuragg",TBC);
+BankAccount nuki = new BankAccount("nuki",TBC);
+
+nuki.DisplayInfo();
+zuragg.DisplayInfo();
+Console.ReadLine();
+nuki.Deposit(100);
+TBC.Transfer(nuki.accountNumber, zuragg.accountNumber, 40);
+nuki.DisplayInfo();
+zuragg.DisplayInfo();
